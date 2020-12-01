83f4c573fe4f6875c345cbbf7eba2f71f8330ef7
Bhupendra singh <me190003017@iiti.ac.in>
2020-12-01 16:59:59 +0530

    add cart in custumor folder in views and login , register in auth

--- server.js
+++ server.js
@@ -7,13 +7,27 @@ const path=require('path');
 
 // Assets
 app.use(express.static('public'));
-app.get("/",(req,res)=>{
-    res.render('home.ejs');
-})
+
 // set Template engine
 app.use(expresslayout);
 app.set('views',path.join(__dirname,'/resources/views'))
 app.set('view engine','ejs');
+
+// Home page is rendering 
+app.get("/",(req,res)=>{
+    res.render('home.ejs');
+})
+// cart page is randering
+app.get("/cart",(req,res)=>{
+    res.render('customers/cart.ejs');
+})
+app.get("/register",(req,res)=>{
+    res.render('auth/register.ejs');
+})
+app.get("/login",(req,res)=>{
+    res.render('auth/login.ejs');
+})
+
 
 app.listen(PORT,()=>{
     console.log(`listening on port xyz ${PORT}`);
